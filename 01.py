3105c52a5a2185313d8b2ecc9a70986f1aaa4ae7
hoangthanhlong <hoangthanhlong@gmail.com>
2016-06-01 13:13:05 +0700

    chmod a+x files

--- 01.py
+++ 01.py
@@ -5,7 +5,7 @@ from base import Problem
 class Solution(Problem):
     def solve(self, input_):
         print('Solving problem {}'.format(self.number))
-        print('Result: {}'.format(sum([i for i in range(input_) if i % 3 == 0 or i % 5 == 0]))
+        print('Result: {}'.format(sum([i for i in range(input_) if i % 3 == 0 or i % 5 == 0])))
     
 
 if __name__ == '__main__':
